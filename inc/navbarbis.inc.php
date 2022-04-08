2ca070a7c11324035bf0979e4fa10ab5c39c5445
Vanusa Santos <vbrasil.tradservices@gmail.com>
2022-04-08 14:44:45 +0200

    maj navbar

--- inc/navbarbis.inc.php
+++ inc/navbarbis.inc.php
@@ -2,9 +2,9 @@
           <p class="lead">Bonjour <?php echo $_SESSION['membre']['prenom']; ?>
               <?php
               if(estAdmin()) { // si le membre est 'admin' il n'a pas les mêmes accès qu'un membre 'client'
-                  echo ' -- Vous êtes administrateur</p>';
+                  echo ' -- Vous êtes administrateur !</p>';
               } else {
-                  echo ' -- Vous êtes connecté rendez-vous à la Boutique</p>';
+                  echo ' -- Vous êtes connecté ! Allez au catalogue !</p>';
               }
               ?>
          
@@ -12,10 +12,10 @@
               <?php 
                   if(estAdmin()) { // si le membre est 'admin' il n'a pas les mêmes accès qu'un membre 'client'
                       echo '<li class="nav-item"><a class="btn btn-primary" href="' .RACINE_SITE. 'admin/accueil.php">Espace admin</a></li>';
-                      echo '<li class="nav-item"><a class="btn btn-success" href="' .RACINE_SITE. 'index.php">Aller à la boutique</a></li>';
+                      echo '<li class="nav-item"><a class="btn btn-success" href="' .RACINE_SITE. 'index.php">Aller au catalogue</a></li>';
                       // echo 'coucou';
                   } else {
-                      echo '<li class="nav-item"><a class="btn btn-success" href="index.php">Retour à la boutique</a></li>';
+                      echo '<li class="nav-item"><a class="btn btn-success" href="index.php">Retour au catalogue</a></li>';
                   }
                   if (estConnecte()) {
                       //  echo 'coucou';
